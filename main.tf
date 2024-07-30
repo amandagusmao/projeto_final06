e7ca7b9faea60d4d69b336b370e52ce603b92318
Amanda Gusmao <engamandagusmao@gmail.com>
2024-07-30 20:06:02 -0300

    Atualizando main.tf

--- main.tf
+++ main.tf
@@ -21,6 +21,30 @@ resource "azurerm_network_security_group" "student_nsg" {
   name                = "student-nsg"
   location            = azurerm_resource_group.student_rg.location
   resource_group_name = azurerm_resource_group.student_rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_public_ip" "student_pip" {
