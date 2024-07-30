95095904e10127671f9d367d79eab0a505bad42e
Amanda Gusmao <engamandagusmao@gmail.com>
2024-07-30 16:20:59 -0300

    Corrigindo arquivo provider.tf

--- provider.tf
+++ provider.tf
@@ -4,6 +4,20 @@ provider "azurerm" {
 
 terraform {
   required_version = ">=0.12"
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
   backend "local" {
     path = "terraform.tfstate"
   }
