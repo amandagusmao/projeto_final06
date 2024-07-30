9d174b360b1fcc9a15665dbc56da53983eefdc21
Amanda Gusmao <engamandagusmao@gmail.com>
2024-07-30 18:12:40 -0300

    Atualizando provider

--- provider.tf
+++ provider.tf
@@ -13,6 +13,10 @@ terraform {
       source  = "hashicorp/random"
       version = "~>3.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
   }
   backend "local" {
     path = "terraform.tfstate"
