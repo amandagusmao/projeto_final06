9cd518955b6f9ae17c757677a5c20b137c9edf30
Amanda Gusmao <engamandagusmao@gmail.com>
2024-07-30 21:11:43 -0300

    Ajustando action

--- main.tf
+++ main.tf
@@ -83,8 +83,8 @@ resource "azurerm_virtual_machine" "student_vm" {
 
   storage_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
 
